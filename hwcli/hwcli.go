fb065fc8840894f05dfc20162fa4b7901df8dc92
linliyuan <linliyuan@37.com>
2023-03-16 17:48:28 +0800

    分开工具版本

--- hwcli/hwcli.go
+++ hwcli/hwcli.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-const Version = "v0.0.3"
+const Version = "v0.0.4"
 
 func main() {
-	fmt.Println("hwcli start " + Version)
+	fmt.Println("hwcli version " + Version)
 }
